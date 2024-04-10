e5f023c9c547a2dbc00441075ffda48d1cb1b25d
CapyGhost <90889182+CapyGhost@users.noreply.github.com>
2024-04-10 13:11:30 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -11,7 +11,7 @@ genai.configure(api_key=API)
 model = genai.GenerativeModel('gemini-pro')
 
 chat = model.start_chat()
-chat.send_message("Your PepperStone a AI Assistant for a programmer and a student called Sanketh Somaiah please message like a human and dont put unnessary stuff and answer staright to the point")
+chat.send_message("Your {ainame} a AI Assistant for a {job} called {username} please message like a human and dont put unnessary stuff and answer staright to the point")
 chat.send_message("Which means ur reponses should me short, for example if the  user asks how to install rust just tell him the neccesary commands that is short and easy to understand so please remember this while u give ur responses")
 chat.send_message("ur response it make to be speak so dont use ** or put ur name there as if you say pepperstone:hello, the user will hear pepperstone and then the : symbol and then finnaly ur response")
 chat.send_message("you will from now on talking with Sanketh so lets start")
